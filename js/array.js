e6f0e39986c5237e96c5005d15a0ce2df0a8eb96
chloehan1122 <hanj951122@gmail.com>
2025-01-02 19:50:34 +0900

    thurs

---
+++ js/array.js
@@ -0,0 +1,120 @@
+// 뉴스
+const newlistArray = [
+  //   <li>
+  //   <a href="#">
+  //     <img src="./img/news-1.png" alt="">
+  //     <p>
+  //       Dmove-Atlassian holds an ITSM hands-on workshop using JSM to provide JSM hands-on training to 70 IT administrative personnel  
+  //     </p>
+  //   </a>
+  // </li>
+  {
+    nId:0,
+    nThumb:"news-1.png",
+    nTitle:``,
+    nDesc:`Dmove-Atlassian holds an ITSM hands-on workshop using JSM to provide JSM hands-on training to 70 IT administrative personnel`,
+    nDate:""
+  },
+  {
+    nId:1,
+    nThumb:"news-2.png",
+    nTitle:``,
+    nDesc:`Achieved the only Jabbix Training Partnership Certificate in Korea,  Dmove is leading the monitoring market`,
+    nDate:""
+  },
+  {
+    nId:2,
+    nThumb:"news-3.png",
+    nTitle:``,
+    nDesc:`Dmove introduces Korea's Largest Cloud Migration cases throughout Atlassian Cloud Migration seminar`,
+    nDate:""
+  },
+  {
+    nId:3,
+    nThumb:"news-4.png",
+    nTitle:``,
+    nDesc:`Achieved Jabbix Partnership Certificate, Dmove builds new Jabbix constructions and provides training and technical support`,
+    nDate:""
+  },
+  {
+    nId:4,
+    nThumb:"news-5.png",
+    nTitle:``,
+    nDesc:`Dmove provides JSM demonstrations based on business environment through ITSM workshop`,
+    nDate:""
+  },
+  {
+    nId:5,
+    nThumb:"news-6.png",
+    nTitle:``,
+    nDesc:`Dmove carries out Atlassian Cloud Migration project of Interpark triple, Korea’s leading ecommerce enterprise`,
+    nDate:""
+  },
+  {
+    nId:6,
+    nThumb:"news-6.png",
+    nTitle:``,
+    nDesc:`Dmove carries out Atlassian Cloud Migration project of Interpark triple, Korea’s leading ecommerce enterprise`,
+    nDate:""
+  },
+  {
+    nId:7,
+    nThumb:"news-6.png",
+    nTitle:``,
+    nDesc:`Dmove carries out Atlassian Cloud Migration project of Interpark triple, Korea’s leading ecommerce enterprise`,
+    nDate:""
+  },
+  {
+    nId:8,
+    nThumb:"news-6.png",
+    nTitle:``,
+    nDesc:`Dmove carries out Atlassian Cloud Migration project of Interpark triple, Korea’s leading ecommerce enterprise`,
+    nDate:""
+  }
+]
+
+// 약관
+// 미적용
+const agreementlistArray = [
+  {
+    par:`Dmove App Privacy & Security
+
+    1.Presemble
+    These terms set out the terms, conditions, procedures and other necessary matters for the use of business-related services (the “Services”) provided by Dmove (the “Company”). This Privacy Policy and Security Policy applies to all of Dmove's products and is intended to inform you about how we treat customer information provided to us and how we collect, process, use, share, control and protect the information you provide to us.We explain how we collect personal information, why we collect it, how we use and share it, the steps we take to protect this information, and how you can exercise your privacy rights. If you do not agree to this Privacy Policy, please do not use our products and services and do not interact with any other aspect of our business. By submitting information to us in any way, using our systems, products or services (the “Services”), or interacting with us through recruitment, events, sales and/or marketing, you consent to the collection, use and transfer of your information.
+
+
+
+    2.Consent to Privacy Policy
+    These terms set out the terms, conditions, procedures and other necessary matters for the use of business-related services (the “Services”) provided by Dmove (the “Company”). This Privacy Policy and Security Policy applies to all of Dmove's products and is intended to inform you about how we treat customer information provided to us and how we collect, process, use, share, control and protect the information you provide to us.We explain how we collect personal information, why we collect it, how we use and share it, the steps we take to protect this information, and how you can exercise your privacy rights. If you do not agree to this Privacy Policy, please do not use our products and services and do not interact with any other aspect of our business. By submitting information to us in any way, using our systems, products or services (the “Services”), or interacting with us through recruitment, events, sales and/or marketing, you consent to the collection, use and transfer of your information.
+
+
+
+    3.Information Protection
+    The Company may change these Terms if there is a reasonable reason, and if the Terms are changed, they shall take effect together with the current Terms by specifying the date of application and the reason for revision.
+
+
+
+    4.Terms Amendment Procedure
+    Members may stop using the Service if they do not agree to the changed Terms. We may update this Privacy Policy from time to time. Any revisions to these terms will be effective immediately upon posting and will apply as a condition of your continued participation with Dmove corporation or continued use of our website, software or services. It is your responsibility to check this page periodically for any updates to this Privacy Policy. We encourage you to review our Privacy Policy each time you use our products or services to stay informed about our information practices and how you can help protect your privacy.
+
+
+
+    5.Compliance with Laws
+    Matters not specified in these Terms shall be governed by the relevant laws and regulations, if any, and otherwise by the Electronic Transactions Consumer Protection Directive and relevant laws and regulations or general practices enacted by the government. This Privacy Policy explains who we are, how we collect personal information, why we collect it, how we use and share it, the steps we take to protect this information, and how you can exercise your privacy rights.
+
+
+
+    6.Agreement to Service Use
+    If you do not agree to this Privacy Policy, please do not use our products and services and do not interact with any other aspect of our business. By submitting information to us in any way, using our systems, products, or services (the “Services”), or interacting with us through recruiting, events, sales, and/or marketing, you consent to the collection, use, and transfer of your information. Dmove will protect your information in accordance with the terms of this Policy.
+
+
+
+    7.Notification of Changes
+    We do not store any personal information and if we make any changes to our Privacy Policy, we will notify you of the changes through a notice on our website or other means. We are constantly working to keep our products and data secure for you.
+
+    `
+  },
+  {
+
+  }
+]
---
+++ js/common.js
@@ -0,0 +1,20 @@
+const setHeaderColor = () => {
+  let winWidth = window.innerWidth; //해상도
+  if(winWidth > 768){
+    // pc버전
+    $(".h-logo img").attr("src","./img/dmove-logo-color.png")
+  } else{
+    // 모바일버전
+    $(".h-logo img").attr("src","./img/dmove-logo.png")
+  }
+}
+
+const bookmark = (num) => {
+  const ulBox = document.querySelector(".partner-wrap");
+  const liBox = ulBox.querySelectorAll("li.brand");
+  let goBox = liBox[num];
+  console.log(goBox)
+  scrRunTop = goBox.offsetTop - 120;
+  //console.log(scrRunTop);
+  window.scrollTo({top: scrRunTop,left: 0,behavior: "smooth"});
+}
